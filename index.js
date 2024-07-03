13c525e53b7ce0eb4f14294527d00c453c1442f3
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-03 23:10:29 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(noOfFeet){
   // Write your code here!
+  if(noOfFeet <=400){
+    return ('This one is on me!')
+  }
+
+  else if (noOfFeet >400 && noOfFeet <=2000) {
+    return("That will be twenty bucks.")
+  }
+
+  else if (noOfFeet >2000 && noOfFeet<=2500){
+    return("I will gladly take your thirty bucks.")
+  }
+
+  else {
+    return("No can do.")
+  }
+  
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city==="NYC"){
+    return("Ok, sounds good.")
+  }
+  else{
+    return("No go.")
+  }
+
+  
+  
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ switch (tip){
+case "generous":
+  return "Thank you so much.";
+case "not as generous":
+  return "Thank you.";
+default: return "Bye."
+ }
 }
